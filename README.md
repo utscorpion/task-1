a192d8cefe1e31599a82411fe9e9fe06eb444bfe
d.hrynko <d.hrynko@godeltech.com>
2018-04-27 18:00:27 +0300

    third commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# First Lab at Godel Technologies
+This is my first lab at Godel Technologies
+
+
+## Geting started
+You can execute it in WEB and in CLI mode
+
+For execute in WEB you must to have Web server
+My  website is accessible by http://mastery.loc address
+Fill in the form, choose your first work day at GodelTach and press button 'send'
+
+For execute in CLI mode you must enter in terminal text 'php /project folder/index.php var1(your name) var2(type = 'integer' - a lesson number) var3(type = 'integer' - amount of mount)'
+
+
+## Versions
+1.0
+not a final version
+to be revision....  
+
+## Author
+Made by D.Hrynko
--- makeDate.php
+++ makeDate.php
@@ -4,6 +4,16 @@
         $c = str_replace($a, $b, $c);
         return $c;
     }
+
+    function exFile ($filename) {
+        if (file_exists($filename)) {
+            $arrFile = file($filename);
+            return $arrFile;
+        } else {
+            echo "Файл $filename не существует";
+        }
+    }
+
     function makeDate ()
     {
         $USERNAME = ucfirst(strtolower($_POST['name']));
@@ -13,8 +23,8 @@
         $MONTHNUM = $_POST['month'];
         $ENDDATE = date("d-m-Y", mktime(0, 0, 0, $arrDate[1] + $MONTHNUM, $arrDate[2], $arrDate[0]));
         $EXECDATE = date("d-m-Y", mktime(0, 0, 0, $arrDate[1], $arrDate[2], $arrDate[0]));
-        $arrFile = file('template.tpl');
-        $toStr = implode('<br>', $arrFile);
+
+        $toStr = implode('<br>', exFile('template.tpl'));
         $regExp = '/[%]\w+[%]/';
         preg_match_all($regExp, $toStr, $matches);
         $arr = $matches[0];
@@ -50,11 +60,16 @@
     {
         $USERNAME = ucfirst(strtolower($arrCon[1]));
         $EXECDATE = date('d-m-Y');
-        $NUMBER = (int)$arrCon[2];
-        $MONTHNUM = (int)$arrCon[3];
+        if((int)$arrCon[2]!=0 && (int)$arrCon[3]!=0) {
+            $NUMBER = (int)$arrCon[2];
+            $MONTHNUM = (int)$arrCon[3];
+        }
+        else {
+            exit('Input correct data');
+        }
         $ENDDATE = date("d-m-Y", mktime(0, 0, 0, date("m")+$MONTHNUM, date("d"),   date("Y")));
         $arrFile = file('template.tpl');
-        $toStr = implode(' ', $arrFile);
+        $toStr = implode(' ', exFile('template.tpl'));
         $regExp = '/[%]\w+[%]/';
         preg_match_all($regExp, $toStr, $matches);
         $arr = $matches[0];
