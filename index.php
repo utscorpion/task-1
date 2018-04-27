37b7e9e3c9f7337b5a0d5e483d585e0c02aa3c4f
d.hrynko <d.hrynko@godeltech.com>
2018-04-27 23:32:24 +0300

    change directory

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-    include_once ('functions.php');
-    include_once ('header.html');
+    include_once('php/functions.php');
+    include_once('html/header.html');
     if (isset($argv)) {
         echo makeDateCons($argv);
         exit;
     } else {
         if (isset($_POST['submit'])) {
             if (isset($_POST['name']) && isset($_POST['date']) && isset($_POST['number']) && isset($_POST['month']) && (int)$_POST['month']!=0 && (int)$_POST['number']!=0) {
-                include_once ('output.php');
+                include_once('php/output.php');
             }
             else {
                 echo 'Check Data';
             }
         }
         else {
-            include_once ('input.html');
+            include_once('html/input.html');
         }
     }
-    include_once ('footer.html');
+    include_once('html/footer.html');
 
 
 
